35cd52dedb3b9bed3e443f19803ebe0ba0ab59cf
sandeepsabu <sandeepsabu321@gmail.com>
2024-04-12 16:51:18 +0530

    added movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -9,18 +9,28 @@ export default async function Movie({params}) {
     return (
 
     <div className="w-full">   
-      <div>
-      <Image 
-          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} 
-          width={500}
-          height={300}
-          className='rounded-lg'
-      />
-      </div>
-      <div>
-        <h2>{movie.title || movie.name}</h2>
-        <p>{movie.overview}</p>
-        <p>{movie.release_date || movie.first_air_date}</p>
+      <div className='p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6'>
+        <Image 
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} 
+            width={500}
+            height={300}
+            className='rounded-lg'
+            style={{maxWidth:'100%' , height:'100%'}}
+        />
+        <div>
+          <h2 className='text-lg mb-3 font-bold '>{movie.title || movie.name}</h2>
+          <p className='mb-3'>
+            {movie.overview}
+          </p>
+          <p className='mb-3'>
+            <span className='font-semibold mr-1'>Date released : </span>
+            {movie.release_date || movie.first_air_date}
+          </p>
+          <p className='mb-3'>
+            <span className='font-semibold mr-1'>Rating : </span>
+            {movie.vote_count}
+          </p>
+        </div>
       </div>
     </div>
   )
