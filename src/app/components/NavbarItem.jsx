99b66044df840efde74c7b67c788b2f88bb62d96
sandeepsabu <sandeepsabu321@gmail.com>
2024-04-12 19:16:06 +0530

    changing corrections

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -1,17 +1,32 @@
 'use client'
 
-import Link from 'next/link'
-import React from 'react'
-import { useSearchParams } from 'next/navigation'
+import React, { Suspense } from 'react';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
-export default function NavbarItem({title,param}) {
+export default function NavbarItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarItemContent title={title} param={param} />
+    </Suspense>
+  );
+}
 
-    const searchParams = useSearchParams();
-    const genre = searchParams.get(`genre`);
+function NavbarItemContent({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
 
   return (
     <div>
-      <Link className={`hover:text-amber-600 font-semibold ${genre==param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg ' :''} `} href={`/?genre=${param}`} >{title}</Link>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
-  )
+  );
 }
+
